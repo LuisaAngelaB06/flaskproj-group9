6c1b9f594d7905986150f79089f7eae94235dc78
LuisaAngelaB06 <lnglblg83@gmail.com>
2025-10-19 19:25:27 +0800

    changed app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_templates
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,12 +6,12 @@ app = Flask(__name__)
 # Login page route
 @app.route('/')
 def login():
-    return render_templates('login-page.html')
+    return render_template('login-page.html')
 
 # Register page route
 @app.route('/register')
 def register():
-    return render_templates('register-page.html')
+    return render_template('register-page.html')
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5055))
